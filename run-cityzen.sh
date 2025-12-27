22406812a2f2a845d6aa4871d58f302cfeb13f18
tashobi_02 <obiditbd@gmail.com>
2025-12-27 18:39:28 +0600

    Updated gitignore, shell script and implemented Dynamic Feed

--- run-cityzen.sh
+++ run-cityzen.sh
@@ -27,6 +27,7 @@ gnome-terminal \
     python -m uvicorn ai_service:app --host 0.0.0.0 --port 8000;
     exec bash"
 
+sleep 45
 # === Terminal 4: Frontend ===
 gnome-terminal \
   --tab --title="CityZen Frontend" -- bash -c "
@@ -34,16 +35,16 @@ gnome-terminal \
     npx expo start -c;
     exec bash"
 
-# === Terminal 5: Gemini Service ===
-gnome-terminal \
-  --tab --title="Gemini Service" -- bash -c "
-    cd gemini-service &&
-    if [ ! -d \"venv\" ]; then
-      python -m venv venv &&
-      source venv/bin/activate &&
-      pip install -r requirements.txt;
-    else
-      source venv/bin/activate;
-    fi &&
-    uvicorn gemini_service:app --host 0.0.0.0 --port 8001;
-    exec bash"
+# # === Terminal 5: Gemini Service ===
+# gnome-terminal \
+#   --tab --title="Gemini Service" -- bash -c "
+#     cd gemini-service &&
+#     if [ ! -d \"venv\" ]; then
+#       python -m venv venv &&
+#       source venv/bin/activate &&
+#       pip install -r requirements.txt;
+#     else
+#       source venv/bin/activate;
+#     fi &&
+#     uvicorn gemini_service:app --host 0.0.0.0 --port 8001;
+#     exec bash"
